2767146af998d8b403497779a53831215d4c0341
user <github email address>
2020-11-01 17:29:51 -0500

    Done.

--- nested.rb
+++ nested.rb
@@ -67,6 +67,7 @@ def adding_matz
 #    :languages => ["LISP", "C"]
 # }
 
+
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -83,9 +84,8 @@ def adding_matz
         }
      }
      
-programmer_hash[:yukihiro_matsumoto] = [:known_for] =>"Ruby", [:languages] => ["LISP","C"]
-
-# programmer_hash[:] =  :yukihiro_matsumoto => {:known_for => "Ruby", :languages => ["LISP", "C"]}   
+programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+programmer_hash
 end
 
 def changing_alan
@@ -107,7 +107,8 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
-     
+programmer_hash[:alan_kay][:known_for] = "GUI"
+programmer_hash
 end
 
 def adding_to_dennis
@@ -126,5 +127,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+h = programmer_hash[:dennis_ritchie]
+new_lang = h[:languages]
+new_lang << "Assembly"
+programmer_hash
 end
