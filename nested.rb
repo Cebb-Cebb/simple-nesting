e98823056701fffd8dcc3beb96df71350a93e104
user <github email address>
2020-11-01 16:19:33 -0500

    Done.

--- nested.rb
+++ nested.rb
@@ -1,4 +1,4 @@
-
+require 'pry'
 def hopper
 	programmer_hash = 
  		{
@@ -16,7 +16,7 @@ def hopper
         }
      }
 
-
+programmer_hash[:grace_hopper]     
 end
 
 def alan_kay_is_known_for
@@ -37,6 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +56,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+h = programmer_hash[:dennis_ritchie][:languages].to_a
+h.join(", ")
 end
 
 def adding_matz
@@ -79,8 +82,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-    
+# programmer_hash[:] =  :yukihiro_matsumoto => {:known_for => "Ruby", :languages => ["LISP", "C"]}   
 end
 
 def changing_alan
